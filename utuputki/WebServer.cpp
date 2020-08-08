9a1a1aa5884d23f3ef13df1f14f9e2beb86e4cb0
Turo Lamminen <turol@iki.fi>
2020-08-08 19:13:27 +0300

    Return templates by value when file override is used

--- utuputki/WebServer.cpp
+++ utuputki/WebServer.cpp
@@ -686,20 +686,20 @@ struct WebServer::WebServerImpl {
 #ifdef OVERRIDE_TEMPLATES
 
 
-	const inja::Template                         &getTemplate(const std::string & /* name */, const inja::Template &defaultTemplate) {
+	inja::Template                               getTemplate(const std::string & /* name */, const inja::Template &defaultTemplate) {
 		// TODO: check file exist, timestamp
 		return defaultTemplate;
 	}
 
-	const inja::Template                         &getPlaylistTemplate() {
+	inja::Template                               getPlaylistTemplate() {
 		return getTemplate("playlist.template", playlistTemplate);
 	}
 
-	const inja::Template                         &getHistoryTemplate() {
+	inja::Template                               getHistoryTemplate() {
 		return getTemplate("history.template", historyTemplate);
 	}
 
-	const inja::Template                         &getListMediaTemplate() {
+	inja::Template                               getListMediaTemplate() {
 		return getTemplate("listMedia.template", listMediaTemplate);
 	}
 
